5f36f67929776011037c0ed5937c5aee18360d56
Fastion-1024 <dsprice1989@outlook.com>
2021-09-30 17:40:19 +0100

    Added Buttons & Music Banks

---
+++ src/lib/Data.js
@@ -0,0 +1,122 @@
+export const keybinds = [
+    {
+        value: 'Q',
+        code: 'KeyQ',
+    },
+    {
+        value: 'W',
+        code: 'KeyW',
+    },
+    {
+        value: 'E',
+        code: 'KeyE',
+    },
+    {
+        value: 'A',
+        code: 'KeyA',
+    },
+    {
+        value: 'S',
+        code: 'KeyS',
+    },
+    {
+        value: 'D',
+        code: 'KeyD',
+    },
+    {
+        value: 'Z',
+        code: 'KeyZ',
+    },
+    {
+        value: 'X',
+        code: 'KeyX',
+    },
+    {
+        value: 'C',
+        code: 'KeyC',
+    },
+];
+
+export const bankOne = {
+    name: 'Bank 1',
+    bank: [
+        {
+            name: 'Heater-1',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        },
+        {
+            name: 'Heater-2',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        },
+        {
+            name: 'Heater-3',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+        },
+        {
+            name: 'Heater-4',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+        },
+        {
+            name: 'Clap',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+        },
+        {
+            name: 'Open-HH',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+        },
+        {
+            name: "Kick-n'Hat",
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+        },
+        {
+            name: 'Kick',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+        },
+        {
+            name: 'Closed-HH',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+        },
+    ],
+};
+
+export const bankTwo = {
+    name: 'Bank 2',
+    bank: [
+        {
+            name: 'Chord-1',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+        },
+        {
+            name: 'Chord-2',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+        },
+        {
+            name: 'Chord-3',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+        },
+        {
+            name: 'Shaker',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+        },
+        {
+            name: 'Open-HH',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+        },
+        {
+            name: 'Closed-HH',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+        },
+        {
+            name: 'Punchy-Kick',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+        },
+        {
+            name: 'Side-Stick',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+        },
+        {
+            name: 'Snare',
+            url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+        },
+    ],
+};
