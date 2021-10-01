9fa6b5356ac72698e9261f2aa14d9acbf5e5bd89
Fastion-1024 <dsprice1989@outlook.com>
2021-10-01 16:43:09 +0100

    Added Components To Display - Added VolumeDisplay. - AddedToggleDisplay. - Added Stylsheet for Display Component and it's children. - Display now uses context hook to access required props.

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import './Display.css';
+import VolumeDisplay from '../VolumeDisplay/VolumeDisplay';
+import ToggleDisplay from '../ToggleDisplay/ToggleDisplay';
+import { useGlobalContext } from '../../hooks/context';
 
-const Display = ({ value }) => {
-    return <div id='display'>{value}</div>;
+const Display = () => {
+    const { display, volume, power, activeBank } = useGlobalContext();
+
+    return (
+        <div id='display-container'>
+            <VolumeDisplay value={volume} disabled={!power} />
+            <ToggleDisplay disabled={!power} activeBankName={activeBank.name} />
+            <h2 id='display'>{display}</h2>
+        </div>
+    );
 };
 
 export default Display;
